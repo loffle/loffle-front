56defff56e9026a67a8161aa5798d93fb3a6d1f5
minbr0ther <minbr0ther@naver.com>
2021-09-09 14:33:52 +0900

    BREAKING CHANGE: change API from jsonplaceholder to loffle

--- src/hooks/usePostFetch.js
+++ src/hooks/usePostFetch.js
@@ -7,14 +7,21 @@ export const usePostFetch = (props) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(8);
   //총 데이터를 postsPerPage 만큼 등분해서 보여줍니다.
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      setLoading(true);
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      setPosts(response.data);
+      try {
+        setLoading(true);
+        const response = await axios.get("/community/post.json");
+        console.log(response.data.results);
+
+        //띄워주는거 일단 성공,
+
+        setPosts(response.data.results);
+      } catch (e) {
+        setError(e);
+      }
       setLoading(false);
     }
     fetchData();
