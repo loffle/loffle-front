0a7ad8abad3f2a709debb0274400315cb5761f5d
minbr0ther <minbr0ther@naver.com>
2021-10-06 20:14:08 +0900

    Chore: Add PROXY in Password

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { PROXY } from '../config';
 
 import Warning from '../Warning';
 
